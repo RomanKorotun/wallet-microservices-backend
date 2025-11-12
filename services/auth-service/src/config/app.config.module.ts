b7966639100a99f6d5b5c881f1485d5d430e7e2d
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-11-12 18:34:41 +0200

    feat(auth): add config module for auth service

---
+++ services/auth-service/src/config/app.config.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
+})
+export class AppConfigModule {}
