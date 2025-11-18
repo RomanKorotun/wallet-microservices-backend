2bf11452d08b51ec868c82491a3c2670f7dc6e91
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-11-18 22:26:09 +0200

    feat(app): add global API prefix

--- services/auth-service/src/main.ts
+++ services/auth-service/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   const logger = new Logger('Bootstrap');
 
   app.useGlobalPipes(
